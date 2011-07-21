a803fa0728a2cdc2df8a3b832aa90584c531bc3d
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-07-21 23:27:08 +0300

    Added dataentry model

---
+++ app/controllers/data_entries_controller.rb
@@ -0,0 +1,83 @@
+class DataEntriesController < ApplicationController
+  # GET /data_entries
+  # GET /data_entries.json
+  def index
+    @data_entries = DataEntry.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @data_entries }
+    end
+  end
+
+  # GET /data_entries/1
+  # GET /data_entries/1.json
+  def show
+    @data_entry = DataEntry.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @data_entry }
+    end
+  end
+
+  # GET /data_entries/new
+  # GET /data_entries/new.json
+  def new
+    @data_entry = DataEntry.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @data_entry }
+    end
+  end
+
+  # GET /data_entries/1/edit
+  def edit
+    @data_entry = DataEntry.find(params[:id])
+  end
+
+  # POST /data_entries
+  # POST /data_entries.json
+  def create
+    @data_entry = DataEntry.new(params[:data_entry])
+
+    respond_to do |format|
+      if @data_entry.save
+        format.html { redirect_to @data_entry, notice: 'Data entry was successfully created.' }
+        format.json { render json: @data_entry, status: :created, location: @data_entry }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @data_entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /data_entries/1
+  # PUT /data_entries/1.json
+  def update
+    @data_entry = DataEntry.find(params[:id])
+
+    respond_to do |format|
+      if @data_entry.update_attributes(params[:data_entry])
+        format.html { redirect_to @data_entry, notice: 'Data entry was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @data_entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /data_entries/1
+  # DELETE /data_entries/1.json
+  def destroy
+    @data_entry = DataEntry.find(params[:id])
+    @data_entry.destroy
+
+    respond_to do |format|
+      format.html { redirect_to data_entries_url }
+      format.json { head :ok }
+    end
+  end
+end
