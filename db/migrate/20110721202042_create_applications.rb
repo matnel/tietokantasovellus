472bd326cca6b1ea5c842e758eb30948b290b2c2
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-07-21 23:23:04 +0300

    Added application model

---
+++ db/migrate/20110721202042_create_applications.rb
@@ -0,0 +1,11 @@
+class CreateApplications < ActiveRecord::Migration
+  def change
+    create_table :applications do |t|
+      t.string, :title
+      t.text, :description
+      t.decimal :version
+
+      t.timestamps
+    end
+  end
+end
