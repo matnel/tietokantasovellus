095d5535e15049c9e1a37101a8279876efb90ac5
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-07 20:34:45 +0300

    Adding file upload & view to models and views

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,7 @@ class Application < ActiveRecord::Base
   has_many :comments
   has_many :data_entries
 
+  mount_uploader :appimage, ImageUploader
+  mount_uploader :appexecutable, ExecutableUploader
+
 end
