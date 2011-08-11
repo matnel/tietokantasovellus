fb609a253ea5b6680559ab4ced6d5e50eb88d2ca
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-11 21:52:16 +0300

    examples of statistic calculation

--- app/models/application.rb
+++ app/models/application.rb
@@ -10,4 +10,18 @@ class Application < ActiveRecord::Base
      return comments.empty?
   end
 
+  def data_number_of_entries(tag)
+      return data_entries_with_tag( tag ).length
+  end
+
+  def data_number_of_ids(tag)
+      return data_entries_with_tag( tag ).group( :id ).length
+  end
+
+  private
+
+  def data_entries_with_tag( tag )
+      return data_entries.where( :key => tag )
+  end
+
 end
