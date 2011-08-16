0e5fa1124c57b1d73f21d40ccbe07e65278058fe
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-16 20:34:19 +0300

    authentication logic for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+require "digest/md5"
+
 class User < ActiveRecord::Base
+
+  def check_password?( password )
+	@password == Digest::MD5.hexdigest( password + @@salt )
+  end
+
+  def password( password )
+        @password = Digest::MD5.hexdigest( password + @@salt )
+  end
+
+  private
+   @@salt = "mun_hyvin_salainen_suola_1234"
+
 end
