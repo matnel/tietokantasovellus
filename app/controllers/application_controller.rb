356aa8c6530a3e0d9468c216a48aea894aef05f9
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-23 10:17:00 +0300

    Typo fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :current_user
 
   def current_user
-	@current_user = User.find(session[:current_user_id])
+	@current_user = User.find(session[:current_user]) if session[:current_user]
   end
 
 end
