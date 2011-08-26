2ba01de09e191acf87c63e8649e1e8f92b0f153b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-08-26 22:07:31 +0300

    Validate that the two passwords are the smae

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,25 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :username
 
-  attr_protected :password_confirm
+  attr_accessor :password_confirm
 
-  before_save :encrypt 
+  validate :passwords_are_same
+
+  before_save :encrypt
+
+  def passwords_are_same
+     if password != password_confirm
+        errors.add( :password, "must be same" )
+        errors.add( :password_confirm, "must be same" )
+     end
+  end
+
+  def name()
+    firstname + " " + lastname
+  end
 
   def check_password?( password )
+        Digest::MD5.hexdigest( self.password + @@salt )
 	self.password == Digest::MD5.hexdigest( password + @@salt )
   end
 
